1eea15a1cdb4f10337ac5200e81b0086942cd4ce
Milauraaa <milaauraaa@gmail.com>
2022-08-25 20:05:40 +0100

    add ed mobile layot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navLinks");
+  const navLinks = document.querySelectorAll(".navLinks li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
 function copyText() {
   let copy = document.getElementById("input");
   copy.select();
